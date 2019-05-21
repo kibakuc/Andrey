54b786ea56213a1e32a8da1880bee55442726eae
kibakuc <a.shchokotov@bankvostok.com.ua>
2019-05-21 08:42:39 +0300

    task13

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -9,15 +9,21 @@ namespace Lab1 {
         static void Main(string[] args) {
             job2(a: 20, b: 20, c: 20, d: 20);
             job4(x: 20, y: 20);
+            job13(x: 20, y: 20);
         }
 
-        private static void job2(Double a=10,Double b=10, Double c= 10,Double d=10) {
+        private static void job2(Double a = 10, Double b = 10, Double c = 10, Double d = 10) {
             Double result = (a / c) * (b / d) - (((a * b) - c) / (c * d));
             Console.WriteLine(result);
         }
 
         private static void job4(Double x = 10, Double y = 10) {
-            Double result = ((x+y)/(y+1))-(((x*y)-12)/(34+x));
+            Double result = ((x + y) / (y + 1)) - (((x * y) - 12) / (34 + x));
+            Console.WriteLine(result);
+        }
+
+        private static void job13(Double x, Double y) {
+            Double result = (Math.Cos(x) / (Math.PI - (2 * x))) + (16 * x * Math.Cos(x * y) - 2);
             Console.WriteLine(result);
         }
     }
