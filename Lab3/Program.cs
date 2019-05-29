f148233b6c2382621d4b77865ae5023854bddbcc
kibakuc <a.shchokotov@bankvostok.com.ua>
2019-05-29 15:02:08 +0300

    edit_2905_1

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -8,24 +8,40 @@ using System.Threading.Tasks;
 namespace Lab3 {
     class Program {
         static void Main(string[] args) {
-            //int length = 10, d=0;
-            //for (int i = 0; i < length; i++) {
-            //    Console.WriteLine(d++);
-            //}
+            Console.WriteLine("8.Умножение числа а = 12345679 на числа 9, 18, 27, ... 81 дает интересные результаты.' Напишите программу получения этих произведений.'");
+            //8.Умножение числа а = 12345679 на числа 9, 18, 27, ... 81 дает интересные результаты.' Напишите программу получения этих произведений.'
+            int x = 12345679;
+            int[] fibarray = new int[] { 9, 18, 27, 36, 45, 54, 63, 72, 81 };
+            foreach (int element in fibarray)
+                Console.WriteLine(element * x);
 
-            BigInteger big1 = new BigInteger(double.MaxValue);
-            BigInteger big2 = new BigInteger(double.MaxValue);
+            Console.WriteLine("9.Одна штука некоторого товара стоит 20,4 грн.Напечатать таблицу стоимости 2, 3, ..., 20 штук …этого товара.");
+            //9.Одна штука некоторого товара стоит 20,4 грн.Напечатать таблицу стоимости 2, 3, ..., 20 штук …этого товара.
+            double x1 = 20.4;
+            double[] fibarray1 = new double[] { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+            foreach (int element in fibarray1)
+                Console.WriteLine(element * x1);
 
-            // Add the 2 values together.
-            BigInteger double1 = BigInteger.Add(big1, big2);
+            Console.WriteLine("13.Напечатать таблицу перевода 1, 2, ..., 20 долларов США в гривни по текущему курсу(курс вводится с клавиатуры)");
+            //13.	Напечатать таблицу перевода 1, 2, ..., 20 долларов США в гривни по текущему курсу (курс вводится с клавиатуры).
+            // Ввести курс гривны с клавиатуры
+            Console.Write("Ввести курс гривны и нажать Enter:\n");
+            // Получение строки курса
+            string kursString = Console.ReadLine().Replace('.', ',');
+            // Преобразование строки в число
+            double kursgrn = Convert.ToDouble(kursString);
+            double[] usdarray = new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+            foreach (int element in usdarray)
+                Console.WriteLine($"{element} USD = {element * kursgrn} UAH");
 
-            // Print the values of the BigIntegers.
-            //Console.WriteLine("DOUBLE MAX: " + big1.ToString());
-            Console.WriteLine("DOUBLE MAX * 2: " + double1.ToString());
-
-            DateTime time = DateTime.Now;             // Use current time.
-            string format = "dd.MM.yyyy HH:mm:ss";   // Use this format.
-            Console.WriteLine(time.ToString(format)); // Write to console.
+            Console.Write("347.Даны натуральные числа m и n.Вычислить 1n + 2n + ... +mn = ");
+            //347.Даны натуральные числа m и n.Вычислить 1n + 2n + ... +mn.
+            int n = 10, m = 11, i = 1;
+            Double r = 0;
+            for (i = 1; i < m; i++) {
+                r += Math.Pow(i, n);
+            }
+            Console.WriteLine($"{r}");
         }
     }
 }
--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -7,39 +7,44 @@ using System.Threading.Tasks;
 namespace Lab4 {
     class Program {
         static void Main(string[] args) {
-            //int[] a1 = new int[5];
-            //int[] a1 = { 1, 3, 5, 7 };
-            //Array arr = a1;
+            Console.WriteLine("221.Удалить из массива все повторяющиеся элементы, оставив их первые вхождения, то есть в массиве должны остаться только разные элементы.");
+            int i = 10;
+            int[] arr;
+            arr = createArr(i, 0, i);
+            Console.WriteLine("Исходный массив:");
+            printArr(arr);
+            Console.WriteLine();
+            Console.WriteLine("Результат:");
+            int[] result = arr.Distinct().ToArray();
+            printArr(result);
+            Console.WriteLine();
 
-            //Console.WriteLine(arr.Rank);
-            //Console.WriteLine(arr.Length);
-            //arr.GetLength
-            //Console.WriteLine(58 % 6);
-            int va = 0, vb = 0, vc = 0, vd = 0;
-            for (int a = 1; a < 100; a++) {
+            Console.WriteLine("223.Данные о росте 25 учеников класса, упорядоченные по убыванию, записаны в массиве. В начале учебного года в класс поступили два новых ученика.");
+            Console.WriteLine("    Получить аналогичный массив, учитывающий рост новых учеников. Каков должен быть максимальный размер исходного массива ?");
+            Console.WriteLine("Исходный массив:");
+            arr = createArr(25, 150, 180);
+            Array.Reverse(arr);
+            printArr(arr);
+            //Console.WriteLine();
+            Array.Resize(ref arr, (25 + 2));
+            arr.SetValue(145, 25);
+            arr.SetValue(156, 26);
+            printArr(arr);
+            Console.WriteLine($"размер массива {arr.Length}");
+        }
 
-                if (a % 3 == 1)
-                    va++;
-                if (a % 4 == 2)
-                    vb++;
-                if (a % 5 == 3)
-                    vc++;
-                if (a % 6 == 4)
-                    vd++;
-                /*
-                if (a % 3 == 1)
-                    if (a % 4 == 2)
-                        if (a % 5 == 3)
-                            if (a % 6 == 4)
-                                Console.WriteLine(a);
-                    */
+        private static void printArr(int[] arr) {
+            foreach (int item in arr) {
+                Console.WriteLine(item);
             }
-            Console.WriteLine($"а) - {va}");
-            Console.WriteLine($"б) - {vb}");
-            Console.WriteLine($"в) - {vc}");
-            Console.WriteLine($"г) - {vd}");
-            Console.WriteLine($"Итого - {va + vb + vc + vd}");
+        }
 
+        private static int[] createArr(int i, int min, int max) {
+            int[] arr = new int[i];
+            Random rnd = new Random();
+            for (i = 0; i < arr.Length; arr[i++] = rnd.Next(min, max))
+                ;
+            return arr;
         }
     }
 }
