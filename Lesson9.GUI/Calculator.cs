369928dd8a34490cb1d8f0ba73538017c6ecb9db
a.shchokotov <a.shchokotov@bankvostok.com.ua>
2019-07-02 10:21:54 +0300

    Lab8_1

---
+++ Lesson9.GUI/Calculator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson9.GUI {
+    class Calculator {
+        double number1, number2;
+        int operationCode;  // 1 - plus, 2 - minus, 3 - mult, 4 - division, 5 - Sqrt, 6- %, 7 - 1/x
+        string result;
+        double memory;
+
+
+        public double Number1 { get => number1; set => number1 = value; }
+        public double Number2 { get => number2; set => number2 = value; }
+        public int OperationCode { get => operationCode; set => operationCode = value; }
+        public string Result { get => result; }
+        public double Memory { get => memory; set => memory = value; }
+
+        public void Calculate() {
+            switch (this.operationCode) {
+                case 1:
+                    result = (number1 + number2).ToString();
+                    break;
+                case 2:
+                    result = (number1 - number2) + "";
+                    break;
+                case 3:
+                    result = (number1 * number2).ToString();
+                    break;
+                case 4:
+                    result = (number2 != 0 ? (number1 / number2).ToString() : "Division by zero");
+                    break;
+                case 5:
+                    result = (number1 >= 0 ? Math.Sqrt(number1).ToString() : "Недопустимый ввод");
+                    break;
+                case 6:
+                    result = (number1 * (number2 / 100)).ToString();
+                    break;
+
+                case 7:
+                    result = (number1 != 0 ? (1 / number1).ToString() : "Division by zero");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ Lesson9.GUI/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lesson9.GUI {
+    public partial class Form1 : Form {
+        Calculator calculator;
+        bool isSecondNumber;
+
+        public Form1() {
+            InitializeComponent();
+            calculator = new Calculator();
+            isSecondNumber = false;
+        }
+
+        public void AddDigit(String digit) {
+            if (display.Text.Equals("0") || isSecondNumber)
+                display.Text = digit;
+            else
+                display.Text += digit;
+
+            isSecondNumber = false;
+        }
+
+        public void ChooseOperation(object sender, EventArgs e) {
+            switch (((Button)sender).Text) {
+                case "+":
+                    calculator.OperationCode = 1;
+                    break;
+                case "-":
+                    calculator.OperationCode = 2;
+                    break;
+                case "*":
+                    calculator.OperationCode = 3;
+                    break;
+                case "/":
+                    calculator.OperationCode = 4;
+                    break;
+                case "√": {
+                        calculator.OperationCode = 5;
+                        calculator.Number1 = Convert.ToDouble(display.Text);
+                        calculator.Calculate();
+                        display.Text = calculator.Result;
+                        break;
+                    }
+                case "%": {
+                        calculator.OperationCode = 6;
+                        calculator.Number2 = Convert.ToDouble(display.Text);
+                        calculator.Calculate();
+                        display.Text = calculator.Result;
+                        break;
+                    }
+                case "1/x": {
+                        calculator.OperationCode = 7;
+                        calculator.Number1 = Convert.ToDouble(display.Text);
+                        calculator.Calculate();
+                        display.Text = calculator.Result;
+                        break;
+                    }
+
+                default:
+                    break;
+            }
+
+            calculator.Number1 = Convert.ToDouble(display.Text);
+            this.isSecondNumber = true;
+
+        }
+
+        private void buttonDigit_Click(object sender, EventArgs e) {
+            AddDigit(((Button)sender).Text);
+        }
+
+        private void button12_Click(object sender, EventArgs e) {
+            double number = Convert.ToDouble(display.Text) * (-1);
+            display.Text = number.ToString();
+        }
+
+        private void button10_Click(object sender, EventArgs e) {
+            if (!display.Text.Contains(","))
+                display.Text += ",";
+        }
+
+        private void button18_Click(object sender, EventArgs e) {
+            display.Text = "0";
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e) {
+            calculator.Number2 = Convert.ToDouble(display.Text);
+            calculator.Calculate();
+            display.Text = calculator.Result;
+        }
+
+        private void buttonMemoryClear_Click(object sender, EventArgs e) {
+            calculator.Memory = 0;
+            labelMemory.Visible = false;
+        }
+
+        private void buttonMemoryRead_Click(object sender, EventArgs e) {
+            display.Text = calculator.Memory.ToString();
+        }
+
+        private void buttonMemoryPlus_Click(object sender, EventArgs e) {
+            calculator.Memory += Convert.ToDouble(display.Text);
+            isSecondNumber = true;
+            if (calculator.Memory != 0)
+                labelMemory.Visible = true;
+            else
+                labelMemory.Visible = false;
+        }
+
+        private void buttonMemoryMinus_Click(object sender, EventArgs e) {
+            calculator.Memory -= Convert.ToDouble(display.Text);
+            isSecondNumber = true;
+            if (calculator.Memory != 0)
+                labelMemory.Visible = true;
+            else
+                labelMemory.Visible = false;
+        }
+
+    }
+}
